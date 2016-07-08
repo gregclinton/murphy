10bcfb132a5ba6477109f78f77eaa8622f70d838
Greg Clinton <greg.clinton@gmail.com>
2016-07-08 07:13:54 -0700

    latest

---
+++ cancer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/probml/pmtk3/master/bigData/14cancer/14cancer.'
+
+def get_train():
+    X = pd.read_fwf(url + 'xtrain', header = None, skiprows = 1).values.T
+    y = pd.read_table(url + 'ytrain', sep = ' ', header = None).ix[0, 1:].values
+    return X, y
+
+def get_test():
+    X = pd.read_fwf(url + 'xtest', header = None, skiprows = 1).values.T
+    y = pd.read_table(url + 'ytest', sep = ' ', header = None).ix[0, 1:].values
+    return X, y
