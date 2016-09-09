2498aba8d292eab471af92a17d644449d95bf455
Greg Clinton <greg.clinton@gmail.com>
2016-09-09 07:10:03 -0700

    latest

--- newton.py
+++ newton.py
@@ -9,19 +9,18 @@ def fit(X, y):
     ybar = np.mean(y)
     w0 = np.log(ybar / (1 - ybar))
 
-    for k in range(2):
+    for k in range(20):
         eta = w0 + X.dot(w)
         mu = sigmoid(eta)
         s = mu * (1 - mu)
         z = eta + (y - mu) / s
         S = np.diag(s)
-        w = inv(X.T.dot(S).dot(X)).dot(X.T).dot(z)    
+        w = inv(X.T.dot(S).dot(X)).dot(X.T).dot(S).dot(z)    
     return w0, w
 
 def predict(model, X):
     w0, w = model
     return (sigmoid(w0 + X.dot(w)) > 0.5) * 1
-
 
 def fit(X, y):
     N, D = X.shape
@@ -30,15 +29,18 @@ def fit(X, y):
         muw = mu(w)
         return -sum(y * np.log(muw) + (1 - y) * np.log(1 - muw))
 
-    mu = lambda w: sigmoid(X.dot(w))
+    ybar = np.mean(y)
+    w0 = np.log(ybar / (1 - ybar))
+    
+    mu = lambda w: sigmoid(w0 + X.dot(w))
     jac = lambda w: X.T.dot(mu(w) - y)
     S = lambda w, mu: np.diag(mu * (1 - mu))
     hess = lambda w: X.T.dot(S(w, mu(w))).dot(X)
 
-    w = np.ones(D)
-    return minimize(NLL, w, jac = jac, hess = hess, method = 'Newton-CG').x
+    w = np.zeros(D)
+    return w0, minimize(NLL, w, jac = jac, hess = hess, method = 'Newton-CG').x
 
 def predict(model, X):
-    w = model
+    w0, w = model
     X.dot(w)
-    return (sigmoid(X.dot(w)) > 0.5) * 1
+    return (sigmoid(w0 + X.dot(w)) > 0.5) * 1
