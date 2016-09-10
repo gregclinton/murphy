0b2cdba2eb05a08298a668374c4409955f3f8ee0
Greg Clinton <greg.clinton@gmail.com>
2016-09-10 15:09:43 -0700

    latest

--- logistic.py
+++ logistic.py
@@ -4,17 +4,19 @@ from sigmoid import sigmoid
 minimize = None
 
 def fit(X, y):   
+    N, D = X.shape
+    penalty = 0.1
+
     def NLL(w):
         muw = mu(w)
-        return -sum(y * np.log(muw) + (1 - y) * np.log(1 - muw))
+        return -sum(y * np.log(muw) + (1 - y) * np.log(1 - muw)) + penalty * w.dot(w)
 
     ybar = np.mean(y)
     w0 = np.log(ybar / (1 - ybar))
     mu = lambda w: sigmoid(w0 + X.dot(w))
-    g = lambda w: X.T.dot(mu(w) - y)
+    g = lambda w: X.T.dot(mu(w) - y) + 2 * penalty * w
     S = lambda w, mu: np.diag(mu * (1 - mu))
-    H = lambda w: X.T.dot(S(w, mu(w))).dot(X)
-    N, D = X.shape
+    H = lambda w: X.T.dot(S(w, mu(w))).dot(X) + 2 * penalty * np.eye(D)
     w = np.zeros(D)
     return w0, minimize(w, NLL, g, H)
 
