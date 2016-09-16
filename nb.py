5d0cc1bf4d3c9c41f258453b388c910a37a29dae
Greg Clinton <greg.clinton@gmail.com>
2016-09-16 09:22:53 -0700

    latest

--- nb.py
+++ nb.py
@@ -1,11 +1,15 @@
 import numpy as np
 import generative
 
-class Classifier(generative.Classifier):
+'''
+naive bayes classifiers (features treated as independent given theta)
+murphy pp. 84 to 89
+'''        
+
+class Bernoulli(generative.Classifier):
     '''
-    naive bayes classifier (features treated as independent given theta)
-    murphy pp. 84 to 89
     binary features only (this implementation)
+    from sklearn.naive_bayes import BernoulliNB
     '''        
     def __init__(self):
         def get_theta(X, y, N, D, C):
@@ -35,3 +39,37 @@ class Classifier(generative.Classifier):
             return log_likelihood
         
         generative.Classifier.__init__(self, get_theta, get_log_likelihood)
+
+
+class Gaussian(generative.Classifier):
+    '''
+    from sklearn.naive_bayes import GaussianNB
+    '''        
+    def __init__(self):
+        def get_theta(X, y, N, D, C):
+            theta = np.empty((C, D))
+            pseudocount = 1
+
+            for c in range(C):
+                X_c = X[y == c]
+                Non = np.sum(X_c == 1, axis = 0)
+                Noff = np.sum(X_c == 0, axis = 0)
+                theta[c, :] = 1.0 * (Non + pseudocount) / (Non + Noff + 2 * pseudocount)
+
+            return theta
+
+        def get_log_likelihood(X, N, D, C, theta):
+            eps = np.spacing(0)
+            not_X = 1 - X
+            log_theta = np.log(theta + eps)
+            log_not_theta = np.log(1 - theta + eps)        
+            log_likelihood = np.empty((N, C))
+
+            for c in range(C):
+                L1 = X * log_theta[c, :]
+                L0 = not_X * log_not_theta[c, :]
+                log_likelihood[:, c] = np.sum(L1 + L0, axis = 1)
+
+            return log_likelihood
+        
+        generative.Classifier.__init__(self, get_theta, get_log_likelihood)        
