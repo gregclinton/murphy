5c99a99728c089dd03033bb13bdf2cfac8fc43a8
Greg Clinton <greg.clinton@gmail.com>
2016-09-24 12:28:10 -0700

    latest

--- softmax.py
+++ softmax.py
@@ -3,11 +3,11 @@ import numpy as np
 def works(x):
     x = np.array(x, dtype = float)
     x = x.T - np.max(x, axis = 1) # prevent overflow
-    e = np.exp(x.T).T
+    e = np.exp(x)
     return x, e, np.sum(e, axis = 0)
 
 def softmax(x):
-    x, e, s = works(x)
+    _, e, s = works(x)
     return (e / s).T
 
 def log_softmax(x):
