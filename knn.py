e201281801007b2bf4147d5b80852d3e823211b6
Greg Clinton <greg.clinton@gmail.com>
2016-10-13 09:10:27 -0700

    latest

--- knn.py
+++ knn.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-aTa = lambda a: np.dot(a, a)
-dist = lambda x, y: aTa(x - y)
-mode = lambda a: np.argmax(np.bincount(a))
+class Classifier:
+    def __init__(self, k = 10):
+        self.k = k
+        
+    def fit(self, X, y):
+        self.theta = X, y
 
-def train(X, y):
-    return X, y
-                       
-def predict(model, x, k, dist = dist):
-    X, y = model
-    nearest = np.argsort([dist(x, xx) for xx in X])[0 : k]
-    return mode(y[nearest])
+    def predict(self, XX):
+        X, y = self.theta
+        aTa = lambda a: np.dot(a, a)
+        dist = lambda x, y: aTa(x - y)
+        mode = lambda a: np.argmax(np.bincount(a.astype(int)))
+        pred = lambda x: mode(y[np.argsort([dist(x, xx) for xx in X])[0 : self.k]])
+        return np.array([pred(x) for x in XX])
