1e0986ffe370a2955c35203f3e5a11e15a8ddd6a
Greg Clinton <greg.clinton@gmail.com>
2016-10-15 07:43:46 -0700

    latest

--- softmax.py
+++ softmax.py
@@ -13,3 +13,6 @@ def softmax(x):
 def log_softmax(x):
     x, _, s = works(x)
     return (x - np.log(s)).T
+
+def softmax_cross_entropy_with_logits(x, y):
+    return np.array([-y[i].dot(lsm) for i, lsm in enumerate(log_softmax(x))])
