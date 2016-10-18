8c3880eeb0a38caf6ca23dabc071a3d42529f2d7
Greg Clinton <greg.clinton@gmail.com>
2016-10-18 09:20:03 -0700

    latest

--- knn.py
+++ knn.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Classifier:
-    def __init__(self, k = 10):
+    def __init__(self, k = 1):
         self.k = k
         
     def fit(self, X, y):
@@ -10,8 +10,8 @@ class Classifier:
     def predict(self, XX):
         X, y = self.theta
         aTa = lambda a: np.dot(a.T, a)
-        dist = lambda x, y: aTa(x - y)
+        dist = lambda x1, x2: aTa(x1 - x2)
         def pred(x):
             closest = np.argsort([dist(x, xx) for xx in X])[0 : self.k]
-            return y[np.argmax(np.bincount(closest))]
+            return y[np.bincount(closest).argmax()]
         return np.array([pred(x) for x in XX])
