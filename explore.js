9c35ab265e7e66ad73ee25396648c015dd407778
Greg Clinton <greg.clinton@gmail.com>
2017-01-07 09:31:53 -0800

    latest

--- explore.js
+++ explore.js
@@ -23,31 +23,6 @@ angular.module('explore').controller('explore', ['$scope', '$http', function ($s
     };
 }]);
 
-angular.module('explore').directive('shortcut', ['$document', function ($document) {
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: true,
-        link: function (scope, element, attrs) {
-            function code(key) {
-                return key === 'enter' ? 13 : key === 'backspace' ? 8 : key.charCodeAt(0);
-            }
-            
-            $document.bind('keypress', function (e) {
-                if (code(attrs.key) === e.which) {
-                    scope.$apply(attrs.keypress);
-                }
-            });
-            
-            $document.bind('keydown', function (e) {
-                if (code(attrs.key) === e.which) {
-                    scope.$apply(attrs.keydown);
-                }
-            });
-        }
-    };
-}]);
-
 angular.module('explore').directive('chart', [function () {
     function link(scope, element, attribute) {
         var json = scope.data,
