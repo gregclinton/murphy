ec4fd04107051cc30616ed9f65d822196fea1c1e
Greg Clinton <greg.clinton@gmail.com>
2017-01-07 14:28:54 -0800

    latest

---
+++ play.js
@@ -0,0 +1,42 @@
+/*global angular,Plotly*/
+
+angular.module('main', []);
+
+angular.module('main').controller('main', ['$scope', '$http', function ($scope, $http) {
+    $scope.charts = [];
+
+    $http({ url: '/optimize/charts' }).then(
+        function (res) {
+            var layout = {xaxis: {}, yaxis: {}},
+                trace = {line: {}},
+                options = { displayModeBar: false, staticPlot: true };
+
+            layout.height = 180;
+            layout.width = 320;
+            layout.showlegend = false;
+            layout.xaxis.showticklabels = false;
+            layout.xaxis.ticks = '';
+            layout.xaxis.autorange = true;
+            layout.xaxis.zeroline = false;
+            layout.xaxis.showgrid = false;
+            layout.yaxis.autorange = true;
+            layout.yaxis.showticklabels = false;
+            layout.yaxis.ticks = '';
+            layout.yaxis.zeroline = false;
+            layout.yaxis.showgrid = false;
+            layout.margin = {t: 2, l: 1, r: 1, b: 2};
+
+            trace.y = res.data;
+            trace.type = 'scatter';
+            trace.mode = 'lines';
+            trace.line.color = 'darkblue';
+            trace.line.width = 1.5;
+
+            Plotly.plot('chart', [trace], layout, options);
+        },
+
+        function (trace) {
+            $scope.error = trace;
+        }
+    );
+}]);
