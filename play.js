b057c2890b4e43adf46e8beb107b8a58c07ecdd3
Greg Clinton <greg.clinton@gmail.com>
2017-01-07 14:36:05 -0800

    latest

--- play.js
+++ play.js
@@ -40,3 +40,28 @@ angular.module('main').controller('main', ['$scope', '$http', function ($scope, 
         }
     );
 }]);
+
+angular.module('main').directive('shortcut', ['$document', function ($document) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: true,
+        link: function (scope, element, attrs) {
+            function code(key) {
+                return key === 'enter' ? 13 : key === 'backspace' ? 8 : key.charCodeAt(0);
+            }
+            
+            $document.bind('keypress', function (e) {
+                if (code(attrs.key) === e.which) {
+                    scope.$apply(attrs.keypress);
+                }
+            });
+            
+            $document.bind('keydown', function (e) {
+                if (code(attrs.key) === e.which) {
+                    scope.$apply(attrs.keydown);
+                }
+            });
+        }
+    };
+}]);
